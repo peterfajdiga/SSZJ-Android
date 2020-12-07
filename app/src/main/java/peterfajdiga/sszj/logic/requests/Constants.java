522e72764803f118c49fc6eb1985068d38cdde04
Peter Fajdiga <peter.fajdiga@gmail.com>
2020-12-07 22:16:26 +0100

    DefinitionRequest: use fran.si for definitions

--- app/src/main/java/peterfajdiga/sszj/logic/requests/Constants.java
+++ app/src/main/java/peterfajdiga/sszj/logic/requests/Constants.java
@@ -9,22 +9,7 @@ import androidx.annotation.NonNull;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-import java.net.URLEncoder;
-
 public final class Constants {
-
-    public static final String url = "https://sszj.herokuapp.com/";
-
-    public static String buildUrl(final String feature, final String query) {
-        try {
-            return url + feature + "?q=" + URLEncoder.encode(query, "UTF-8");
-        } catch (java.io.UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
     private static RequestQueue queue = null;
 
     public static RequestQueue initQueue(@NonNull Context context) {
--- app/src/main/java/peterfajdiga/sszj/logic/requests/DefinitionRequest.java
+++ app/src/main/java/peterfajdiga/sszj/logic/requests/DefinitionRequest.java
@@ -7,15 +7,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
-public class DefinitionRequest extends StringRequest {
+import org.w3c.dom.Document;
 
-    private static final String feature = "sskj";
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class DefinitionRequest extends StringRequest {
+    private static final String entryMatchStart = "<div class=\"col-sm-12 col-md-8 col-md-push-4 entry\">";  // TODO: use regex, ignore classes other than "entry"
+    private static final String entryMatchEnd = "</div>";
 
     public DefinitionRequest(final Owner owner, final String word) {
-        super(Method.GET, Constants.buildUrl(feature, word), new Listener(owner), new ErrorListener(owner));
+        super(Method.GET, buildUrl(word), new Listener(owner), new ErrorListener(owner));
         setTag(owner);
     }
 
+    private static String buildUrl(final String word) {
+        return "https://www.fran.si/iskanje?FilteredDictionaryIds=133&View=3&Headword=" + word;
+    }
 
     private static class Listener implements Response.Listener<String> {
         private Owner requestOwner;
@@ -25,17 +32,20 @@ public class DefinitionRequest extends StringRequest {
         }
 
         @Override
-        public void onResponse(String response) {
+        public void onResponse(final String response) {
+            final int indexStart = response.indexOf(entryMatchStart) + entryMatchStart.length();
+            final int indexEnd = response.indexOf(entryMatchEnd, indexStart);
+            final String definitionHtml = response.substring(indexStart, indexEnd);
+
             Spanned definition;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                definition = Html.fromHtml(response, Html.FROM_HTML_MODE_COMPACT);
+                definition = Html.fromHtml(definitionHtml, Html.FROM_HTML_MODE_COMPACT);
             } else {
-                definition = Html.fromHtml(response);
+                definition = Html.fromHtml(definitionHtml);
             }
             requestOwner.onWordDefinitionLoaded(definition);
         }
     }
-
 
     private static class ErrorListener implements Response.ErrorListener {
         private Owner requestOwner;
