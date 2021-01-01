c26dc87810e728ba717103ef3de977b185a6acd8
Peter Fajdiga <peter.fajdiga@gmail.com>
2021-01-01 16:32:35 +0100

    Constants: remove queue

--- app/src/main/java/peterfajdiga/sszj/logic/requests/Constants.java
+++ app/src/main/java/peterfajdiga/sszj/logic/requests/Constants.java
@@ -4,28 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
-import androidx.annotation.NonNull;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-
 public final class Constants {
-    private static RequestQueue queue = null;
-
-    public static RequestQueue initQueue(@NonNull Context context) {
-        if (queue == null) {
-            queue = Volley.newRequestQueue(context);
-        }
-        return queue;
-    }
-
-    public static RequestQueue getQueue() {
-        if (queue == null) {
-            throw new RuntimeException("Queue has not yet been initialized.");
-        }
-        return queue;
-    }
-
     public static void openWebsite(final Context context, final String url) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         context.startActivity(browserIntent);
